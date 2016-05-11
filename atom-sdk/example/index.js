5cc91ae5e9df3d99d3143dc72898a9e8d3199f2e
Evhen Popenko <evhen.popenko@ironsrc.com>
2016-05-11 18:52:55 +0300

    update example

--- atom-sdk/example/index.js
+++ atom-sdk/example/index.js
@@ -5,89 +5,108 @@
     endpoint: "https://track.atom-data.io/",
     auth: "YOUR_API_KEY"
   };
+  var stream = "",
+      httpMethod = "POST";
 
   var atom = new IronSourceAtom(options);
 
   var sendEventBtn  = document.getElementById('track-event'),
       sendEventsBtn  = document.getElementById('track-events'),
-      addData = document.getElementById('add-data'),
-      updateOptions = document.getElementById('update-options'),
-      resetOptions = document.getElementById('reset-options');
+      addData = document.getElementById('add-data');
+      // updateOptions = document.getElementById('update-options'),
+      // resetOptions = document.getElementById('reset-options');
 
   var count = document.getElementById('events-count'),
       optionsDisplay = document.getElementById('options-display'),
+      responseDisplay = document.getElementById('response-display'),
       dataInput = document.getElementById('input-data'),
-      authInput = document.getElementById('auth'),
-      endpointInput = document.getElementById('endpoint'),
+      methodInput = document.getElementsByName('method'),
+      streamInput = document.getElementById('stream'),
       codeDisplay = document.getElementById('bulk');
   
   var data = [];
 
   updateOptionsDisplay();
 
-  authInput.addEventListener('blur', function() {
-    options.auth = this.value;
-  });
+  for(var i=0; i < methodInput.length; i++){
+    methodInput[i].addEventListener('click', function() {
+      httpMethod = this.value;
+      updateOptionsDisplay();
+    });
+  }
 
-  endpointInput.addEventListener('blur', function() {
+  streamInput.addEventListener('blur', function() {
     if (this.value != "") {
-      options.endpoint = this.value;
+      stream = this.value;
+      updateOptionsDisplay();
     }
   });
 
-  updateOptions.addEventListener('click', function() {
-    setTimeout(function() {
-      atom = new IronSourceAtom(options);
-
-      updateOptionsDisplay();
-
-      // health method for check is server alive
-      atom.health(function(res){
-        console.log('Is server alive? - ', !!res && res.status < 500 && res.status != 404);
-      });
-    }, 50);
-  });
-  resetOptions.addEventListener('click', function() {
-    setTimeout(function() {
-      options = {
-        endpoint: "https://track.atom-data.io/",
-        apiVersion: "V1",
-        auth: "YOUR_API_KEY"
-      };
-
-      atom = new IronSourceAtom(options);
-
-      updateOptionsDisplay();
-
-      // health method for check is server alive
-      atom.health(function(res){
-        console.log('Is server alive? - ', !!res && res.status < 500 && res.status != 404);
-      });
-    }, 50);
-  });
+  // updateOptions.addEventListener('click', function() {
+  //   setTimeout(function() {
+  //     atom = new IronSourceAtom(options);
+  //
+  //     updateOptionsDisplay();
+  //
+  //     // health method for check is server alive
+  //     atom.health(function(res){
+  //       console.log('Is server alive? - ', !!res && res.status < 500 && res.status != 404);
+  //     });
+  //   }, 50);
+  // });
+  // resetOptions.addEventListener('click', function() {
+  //   setTimeout(function() {
+  //     options = {
+  //       endpoint: "https://track.atom-data.io/",
+  //       apiVersion: "V1",
+  //       auth: "YOUR_API_KEY"
+  //     };
+  //
+  //     atom = new IronSourceAtom(options);
+  //
+  //     updateOptionsDisplay();
+  //
+  //     // health method for check is server alive
+  //     atom.health(function(res){
+  //       console.log('Is server alive? - ', !!res && res.status < 500 && res.status != 404);
+  //     });
+  //   }, 50);
+  // });
 
   
   // Add putEvent(params, callback) params {object}, callback {function}
   sendEventBtn.addEventListener('click', function(){
-    atom.putEvent({ data: "{\"name\": \"iron\", \"last_name\": \"Source\"}",
-        table: "yourStreamName",
-        method: "GET" // optional, default POST
-      },
-      function(res){
-        console.log(res);
-      }); 
+    try {
+      atom.putEvent({ data: "{\"name\": \"iron\", \"last_name\": \"Source\"}",
+          table: stream,
+          method: httpMethod
+        },
+        function(res){
+          displayResponse(res);
+        });
+    } catch (e) {
+      displayError(e);
+    }
+
   });
 
   // Add putEvent(params, callback) params {object}, callback {function}
   sendEventsBtn.addEventListener('click', function() {
-    atom.putEvents({ data: data,
-      table: "yourStreamName",
-      method: "POST"
-    },
-      function(res){
-        console.log(res);
-        count.innerHTML = data.length;
-      });
+    try {
+      atom.putEvents({ data: data,
+          table: stream,
+          method: httpMethod
+        },
+        function(res){
+          displayResponse(res);
+          data = [];
+          count.innerHTML = data.length;
+          codeDisplay.innerHTML = "[]";
+        });
+    } catch (e) {
+      displayError(e);
+    }
+
   });
   
   addData.addEventListener('click', function(){
@@ -95,14 +114,22 @@
     
     data.push(dataInput.value);
     count.innerHTML = data.length;
-    codeDisplay.innerHTML = data.join('\n');
+    codeDisplay.innerHTML = "[" + data.join(',\n') + "]";
   });
 
 
   function updateOptionsDisplay() {
     optionsDisplay.innerHTML = '{ <br>' +
-      '  endpoint: "' + options.endpoint + '",<br>' +
-      '  auth: "' + options.auth +'"<br>}';
+      '  streamName: "' + stream + '",<br>' +
+      '  method: "' + httpMethod +'"<br>}';
+  }
+
+  function displayResponse(res) {
+    responseDisplay.innerHTML = JSON.stringify(res);
+  }
+
+  function displayError(e) {
+    responseDisplay.innerHTML = e;
   }
 
 })();
--- dist/sdk.js
+++ dist/sdk.js
@@ -6,7 +6,6 @@
  * 
  * @param {Object} opt
  * @param {String} opt.endpoint - Endpoint api url
- * @param {String} opt.apiVersion - SDK version
  * @param {String} opt.auth (optional) - auth key for authentication
  *
  * @constructor new IronSourceAtom(options = {}) => Object
@@ -71,7 +70,8 @@ function IronSourceAtom(opt) {
 
 IronSourceAtom.prototype.putEvent = function (params, callback) {
   params = params || {};
-  if (!params.data || !params.table) throw new Error('Data and table is required');
+  if (!params.table) throw new Error('Stream is required');
+  if (!params.data) throw new Error('Data is required');
 
   params.apiVersion = this.options.apiVersion;
   params.auth = this.options.auth;
@@ -133,8 +133,11 @@ IronSourceAtom.prototype.putEvent = function (params, callback) {
 
 IronSourceAtom.prototype.putEvents = function (params, callback) {
   params = params || {};
-  if (!params.data || !(params.data instanceof Array) || !params.table || !params.data.length) {
-    throw new Error('Data (must be not empty array) and table is required');
+  if (!params.data || !(params.data instanceof Array) || !params.data.length) {
+    throw new Error('Data (must be not empty array) is required');
+  }
+  if (!params.table) {
+    throw new Error('Table is required');
   }
 
   params.apiVersion = this.options.apiVersion;
