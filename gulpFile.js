38f15ccb53608d52547066fa53b9cefbb5b5d7f3
Evhen Popenko <evhen.popenko@ironsrc.com>
2016-05-13 18:06:32 +0300

    add travic, bower, gulp wrap self execute fn

--- gulpFile.js
+++ gulpFile.js
@@ -4,10 +4,12 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var selfExecute = require('gulp-self-execute');
 
 gulp.task('js', function () {
   return gulp.src('atom-sdk/src/*.js')
-    .pipe(concat('sdk.js'))
+    .pipe(concat('sdk.js'))    
+    .pipe(selfExecute({}))
     .pipe(gulp.dest('dist/'))
     .pipe(rename('sdk.min.js'))
     .pipe(minify())
