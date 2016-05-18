7814324f532702409337db42da98c3ff6961635c
Zeev Manilovich <zeevmanilovich@gmail.com>
2016-05-18 03:18:07 +0300

    fixing versioning

--- api_project.js
+++ api_project.js
@@ -1,1 +1,15 @@
-define({  "title": "ApiDoc for IronSource Atom JS SDK",  "name": "IronSource Atom JS SDK",  "version": "1.0.0",  "description": "ApiDoc for IronSource Atom JS SDK",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-05-10T14:55:11.888Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
+define({
+  "name": "IronSource Atom JS SDK",
+  "version": "1.0.0",
+  "description": "ApiDoc for IronSource Atom JS SDK",
+  "title": "ApiDoc for IronSource Atom JS SDK",
+  "url": "https://ironsource.github.io/atom-javascript/",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-05-18T00:08:40.331Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
