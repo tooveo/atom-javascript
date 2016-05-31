af566f48094b230a8f1bacce726844af21d66ab0
Evhen Popenko <evhen.popenko@ironsrc.com>
2016-05-31 11:49:48 +0300

    Deploy to GitHub Pages:

--- api_project.js
+++ api_project.js
@@ -1,15 +1,1 @@
-define({
-  "name": "IronSource Atom JS SDK",
-  "version": "1.0.0",
-  "description": "ApiDoc for IronSource Atom JS SDK",
-  "title": "ApiDoc for IronSource Atom JS SDK",
-  "url": "https://ironsource.github.io/atom-javascript/",
-  "sampleUrl": false,
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2016-05-18T00:08:40.331Z",
-    "url": "http://apidocjs.com",
-    "version": "0.16.1"
-  }
-});
+define({  "name": "IronSource Atom JS SDK",  "version": "1.0.1",  "description": "ApiDoc for IronSource Atom JS SDK",  "title": "ApiDoc for IronSource Atom JS SDK",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-05-31T08:49:47.940Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
