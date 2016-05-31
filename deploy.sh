0ef0dc1dfee75e157facd4f5316005d338a6774b
zeevmanilovich <zeev.manilovich@ironsrc.com>
2016-05-31 16:24:13 +0300

    fixing deploy docs script and adding comments

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
+# Small script to auto-deploy docs to gh-pages branch
 set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-rm -rf out/
-git clone -b $TARGET_BRANCH --single-branch https://github.com/ironSource/atom-javascript.git out
-apidoc -i atom-sdk/ -o out/
-cd out
+rm -rf apidocs/
+git clone -b $TARGET_BRANCH --single-branch https://github.com/ironSource/atom-javascript.git apidocs
+apidoc -i atom-sdk/ -o apidocs/
+cd apidocs
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push
-rm -rf out/
+cd ..
+rm -rf apidocs
